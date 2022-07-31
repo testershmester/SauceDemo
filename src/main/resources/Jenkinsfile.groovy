d9311faae02b3db83d9f020b13346864f115dc52
testershmester <katerina.zharskaya@mail.ru>
2022-08-01 02:19:32 +0300

    jenkins: update jenkins file

--- src/main/resources/Jenkinsfile.groovy
+++ src/main/resources/Jenkinsfile.groovy
@@ -7,20 +7,23 @@ pipeline {
     }
 
     parameters {
+        credentials credentialType: 'com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl', defaultValue: '66a23836-a761-45e9-9fe0-c6be97aab61a', name: 'CREDS', required: false
         gitParameter branchFilter: 'origin/(.*)', defaultValue: 'master', name: 'BRANCH', type: 'PT_BRANCH'
     }
 
     stages {
         stage('Build') {
-            steps {
-                // Get some code from a GitHub repository
-                git branch: "${params.BRANCH}", url: 'https://github.com/testershmester/SauceDemo.git'
+            withCredentials([string(credentialsId: '${params.CREDS}', usernameVariable: 'username_var', passwordVariable: 'password_var')]) {
+                steps {
+                    // Get some code from a GitHub repository
+                    git branch: "${params.BRANCH}", url: 'https://github.com/testershmester/SauceDemo.git'
 
-                // Run Maven on a Unix agent.
-                // sh "mvn clean test -DsuiteXmlFile=src/main/resources/smoke.xml"
+                    // Run Maven on a Unix agent.
+                    // sh "mvn clean test -DsuiteXmlFile=src/main/resources/smoke.xml"
 
-                // To run Maven on a Windows agent, use
-                bat "mvn clean test -DsuiteXmlFile=src/main/resources/smoke.xml"
+                    // To run Maven on a Windows agent, use
+                    bat "mvn clean test -DsuiteXmlFile=src/main/resources/smoke.xml"
+                }
             }
         }
         stage('Allure') {
